506f41dc307d3d869bce118e55fc53336a07c60b
Loganou974 <42536967+Loganou974@users.noreply.github.com>
2023-11-25 12:06:54 +0400

    MoveTarget ok, FinalGoal Ok, Lava KillBuddy OK, GameManager OK ité1

---
+++ Assets/GameManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int lastingBuddiesCount;
+    public List<Buddy> lastingBuddies;
+    public bool gameOver;
+
+    public static GameManager Instance; 
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+
+    private void Start()
+    {
+        lastingBuddies.AddRange(FindObjectsByType<Buddy>(FindObjectsSortMode.None));
+        UpdateBuddyCount();
+    }
+
+
+    public void KillBuddy(Buddy target)
+    {
+        lastingBuddies.Remove(target);
+        Destroy(target.gameObject);
+
+        UpdateBuddyCount();
+    }
+
+    void UpdateBuddyCount()
+    {
+        lastingBuddiesCount = lastingBuddies.Count;
+
+        if(lastingBuddiesCount <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        Debug.Log("Game is over");
+        gameOver = true;
+    }
+
+    public void Win()
+    {
+        //win
+        Debug.Log("Victory !");
+    }
+
+}
